4a3ea47e399eb811d0e882c6434e1ec2c990b99b
Lê Công Dũng <44793409+congdung2498@users.noreply.github.com>
2020-03-31 20:45:59 +0700

    spring jwt

---
+++ Spring+jwt/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public interface UserService {
+
+	public User findById(long id);
+
+	public String add(User user);
+
+	public User loadUserByUsername(String username);
+
+	public boolean delete(long id);
+
+	public boolean checkLogin(User user);
+
+	public boolean update(User user);
+
+	public boolean chagePass(User user);
+        
+        public List<User> getAll();
+        
+  }
